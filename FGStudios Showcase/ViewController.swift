63a044490e931ca4d48b4e7399a288c1d5d88939
Joshua Ide <Josh@192-168-1-101.tpgi.com.au>
2016-03-24 23:20:04 +1100

    Signup screen now created Firebase users

--- FGStudios Showcase/ViewController.swift
+++ FGStudios Showcase/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
+        //Check if we alread have the user's login details and skip sign in
         if NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) != nil {
             self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
         }
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
     
     //Actions
     @IBAction func onFacebookBtnPressed(sender: UIButton!) {
+        
         let facebookLogin = FBSDKLoginManager()
 
         facebookLogin.logInWithReadPermissions(["email"], fromViewController: self) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) in
@@ -42,13 +45,19 @@ class ViewController: UIViewController {
                 print("Facebook login failed.  Error \(facebookError)")
             } else {
                 let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
-                print("Successfully logged in with Facebook!  \(accessToken)")
+                print("Successfully obtained Facebook permission!  \(accessToken)")
              
                 DataService.ds.REF_BASE.authWithOAuthProvider("facebook", token: accessToken, withCompletionBlock: { error, authData in
                     if error != nil {
                         print("Login failed. \(error)")
                     } else {
                         print("Logged in! \(authData)")
+                        
+                        //Create the user on Firebase
+                        let user = ["provider": authData.provider!, "blah": "test"]
+                        DataService.ds.createFirebaseUser(authData.uid, user: user)
+                        
+                        //Assign the UID so we can skip login next time app opened
                         NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
                         self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                     }
@@ -60,28 +69,43 @@ class ViewController: UIViewController {
     
     @IBAction func onEmailBtnPressed(sender: UIButton!) {
         if let email = txtEmail.text where email != "", let pwd = txtPassword.text where pwd != "" {
+            
             DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { error, authData in
+                
                 if error != nil {
+                    //The user doesn't exist, create one
                     if error.code == STATUS_ACCOUNT_NONEXIST {
                         DataService.ds.REF_BASE.createUser(email, password: pwd, withValueCompletionBlock: { error, result in
+                            
                             if error != nil {
                                 self.showErrorAlert("Could not create account", msg: "There was a problem creating the account.  Please try again.")
                             } else {
                                 NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
-                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { err, authData in
+                                    
+                                    //Create the user on Firebase
+                                    let user = ["provider": authData.provider!, "blah": "emailtest"]
+                                    DataService.ds.createFirebaseUser(authData.uid, user: user)
+                                })
+                                
                                 self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                             }
                         })
+                    //Email was invalid
                     } else if error.code == STATUS_EMAIL_INVALID {
                         self.showErrorAlert("Invalid Email", msg: "Your email address was invalid.  Please try again.")
+                    //Password was wrong
                     } else if error.code == STATUS_PASSWORD_INVALID {
                         self.showErrorAlert("Invalid Password", msg: "Your password was incorrect.  Please try again.")
                     }
+                //No problems, login
                 } else {
+                    //Assign the UID so we can skip login next time app opened
                     NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
                     self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                 }
             })
+            
         } else {
             showErrorAlert("Empty fields", msg: "Please ensure you enter an email and password.")
         }
