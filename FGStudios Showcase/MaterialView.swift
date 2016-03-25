646a3cb60c801d182d7cb2db5d8590875064dba9
Joshua Ide <Josh@192-168-1-100.tpgi.com.au>
2016-03-25 16:15:55 +1100

    Post FeedVC ui added

--- FGStudios Showcase/MaterialView.swift
+++ FGStudios Showcase/MaterialView.swift
@@ -11,7 +11,7 @@ import UIKit
 class MaterialView: UIView {
 
     override func awakeFromNib() {
-        layer.cornerRadius = layer.bounds.width / 124
+        layer.cornerRadius = layer.bounds.width / (124 * 2)
         layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.8).CGColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
